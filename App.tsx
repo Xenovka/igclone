1772d15e82711a7cbbe2391038107bc200429e0e
Wendy <wendyanto.bukitvista@gmail.com>
2024-03-27 21:17:45 +0700

    Creating the UI for Edit Profile page

--- App.tsx
+++ App.tsx
@@ -3,13 +3,15 @@ import {StyleSheet, View} from 'react-native';
 import HomeScreen from './src/screens/HomeScreen/HomeScreen';
 import CommentsScreen from './src/screens/CommentsScreen/CommentsScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
+import EditProfileScreen from './src/screens/EditProfileScreen';
 
 const App = () => {
   return (
     <View style={styles.app}>
       {/* <HomeScreen /> */}
       {/* <CommentsScreen /> */}
-      <ProfileScreen />
+      {/* <ProfileScreen /> */}
+      <EditProfileScreen />
     </View>
   );
 };
---
+++ src/screens/EditProfileScreen/EditProfileScreen.tsx
@@ -0,0 +1,78 @@
+import {View, Text, StyleSheet, Image, TextInput} from 'react-native';
+
+import user from '../../assets/data/user.json';
+
+import colors from '../../theme/colors';
+import fonts from '../../theme/fonts';
+
+interface ICustomInput {
+  label: string;
+  multiline?: boolean;
+}
+
+const CustomInput = ({label, multiline = false}: ICustomInput) => (
+  <View style={styles.inputContainer}>
+    <Text style={styles.label}>{label}</Text>
+    <TextInput placeholder={label} style={styles.input} multiline={multiline} />
+  </View>
+);
+
+const EditProfileScreen = () => {
+  const onSubmit = () => {
+    console.log('Submit');
+  };
+
+  return (
+    <View style={styles.page}>
+      <Image source={{uri: user.image}} style={styles.avatar} />
+      <Text style={styles.textButton}>Change profile photo</Text>
+
+      <CustomInput label="Name" />
+      <CustomInput label="Username" />
+      <CustomInput label="Website" />
+      <CustomInput label="Bio" multiline />
+
+      <Text style={styles.textButton} onPress={onSubmit}>
+        Submit
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  page: {
+    alignItems: 'center',
+    padding: 10,
+  },
+
+  avatar: {
+    width: '30%',
+    aspectRatio: 1,
+    borderRadius: 100,
+  },
+
+  textButton: {
+    color: colors.primary,
+    fontSize: fonts.size.md,
+    fontWeight: fonts.weight.semi,
+    margin: 10,
+  },
+
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+  },
+
+  label: {
+    width: 100,
+  },
+
+  input: {
+    flex: 1,
+    borderColor: colors.border,
+    borderBottomWidth: 1,
+  },
+});
+
+export default EditProfileScreen;
