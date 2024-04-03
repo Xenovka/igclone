a9c14444d98c5ca696852b3a63491809e341b0f4
Wendy <wendyanto.bukitvista@gmail.com>
2024-04-03 18:59:21 +0700

    setting up aws amplify for the backend

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import {StyleSheet, View} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {Amplify} from 'aws-amplify';
+import config from './src/aws-exports';
 
-// import HomeScreen from './src/screens/HomeScreen/HomeScreen';
-// import CommentsScreen from './src/screens/CommentsScreen/CommentsScreen';
-// import ProfileScreen from './src/screens/ProfileScreen';
-// import EditProfileScreen from './src/screens/EditProfileScreen';
-// import PostUploadScreen from './src/screens/PostUploadScreen';
 import Navigation from './src/navigation';
+
+Amplify.configure(config);
 
 const App = () => {
   return (
